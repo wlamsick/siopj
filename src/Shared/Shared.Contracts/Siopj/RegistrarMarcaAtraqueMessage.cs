20895994ce379a5c12ebd15382cf117fda2bc20a
Walter Lamsick Alguera <walter.lamsick@outlook.com>
2024-08-09 15:25:16 -0600

    chore: Remove unused CodigoOperacion property in RegistrarMarcaAtraqueMessage

--- src/Shared/Shared.Contracts/Siopj/RegistrarMarcaAtraqueMessage.cs
+++ src/Shared/Shared.Contracts/Siopj/RegistrarMarcaAtraqueMessage.cs
@@ -5,5 +5,4 @@ public record RegistrarMarcaAtraqueMessage : IEventMessage
     public int NumeroAZ { get; init; }
     public DateTime Fecha { get; init; }
     public string Puesto { get; init; } = default!;
-    public int CodigoOperacion { get; init; }
 }
